3390d19857d45801d992c1b5ce00462a3b98aedb
dbutov <danyil.butov@gmail.com>
2026-08-05 23:59:09 +0100

    fix: transport

--- internal/transport/transport.go
+++ internal/transport/transport.go
@@ -14,12 +14,20 @@ func Listen(socket string) (net.Listener, error) {
 	}
 
 	if err := os.Chmod(socket, 0o600); err != nil {
-		return nil, fmt.Errorf("failed to change the persmission to the socket %v", err)
+		l.Close()
+		return nil, fmt.Errorf("failed to change the permission to the socket %v: %w", socket, err)
 	}
 
 	return l, nil
 }
 
 func Dial(ctx context.Context, socket string) (net.Conn, error) {
-	return net.Dial("unix", socket)
+	var d net.Dialer
+
+	conn, err := d.DialContext(ctx, "unix", socket)
+	if err != nil {
+		return nil, fmt.Errorf("dial %s: %w", socket, err)
+	}
+
+	return conn, nil
 }
